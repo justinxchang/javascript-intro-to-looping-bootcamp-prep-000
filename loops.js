4c22b061d8bf9ed803d024a9094110233f753ece
Justin Chang <justinxchang@hotmail.com>
2018-08-20 19:52:24 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -24,7 +24,7 @@ function incrementVariable() {
 
 function doWhileLoop(array){
   do {
-    array.shift();
+    array.pop();
     incrementVariable();
    } while (array.length > 0 && incrementVariable())
 }
